651e041b6205d04610d7ea077e5806574ae1e667
lelegod <2021m622@kuas.ac.jp>
2026-02-11 10:37:57 +0100

    add week1 files

---
+++ week1/FamilyDB.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS family;
+CREATE TABLE family(
+   PersonName VARCHAR(40) NOT NULL, 
+   Birthday int, 
+   PRIMARY KEY (PersonName));
+   INSERT INTO family VALUES ('Henry', 19591006);
+   INSERT INTO family VALUES ('Lilly', 19940224);
+   
