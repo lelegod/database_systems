c2301055203fbbd29542643fc7c79a1f1d58e46b
lelegod <2021m622@kuas.ac.jp>
2026-05-24 16:16:34 +0200

    week12 and test exam part 1 answers

--- week12/exam-prep-part-1.sql
+++ week12/exam-prep-part-1.sql
@@ -1,5 +1,5 @@
 -- In next line, insert your student ID after the colon.
--- Student ID: 
+-- Student ID: s252786
 
 -- Below, you must at most make one SQL query for each question. If you make several, they will be ignored.
 
@@ -8,19 +8,37 @@ USE PetShelters;
 -- -----------------------------------------------------------------------------------------------------
 -- q1: Answer to question 1 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+SELECT breed, AVG(age)
+FROM Pet LEFT JOIN Shelter USING (ShelterID)
+WHERE City = 'Lyngby'
+GROUP BY breed;
 -- -----------------------------------------------------------------------------------------------------
 -- q2: Answer to question 2 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+SELECT personName
+FROM Pet JOIN Person ON Person.personName = Pet.petName;
 -- -----------------------------------------------------------------------------------------------------
 -- q3: Answer to question 3 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+SELECT petName, COUNT(personname) AS numReservations
+FROM Pet NATURAL LEFT JOIN reservedFor
+GROUP BY petName
+ORDER BY numReservations DESC;
 -- -----------------------------------------------------------------------------------------------------
 -- q4: Answer to question 4 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+DELIMITER //
+CREATE TRIGGER reservedFor_Before_Insert
+BEFORE INSERT ON reservedFor FOR EACH ROW
+BEGIN
+	IF NEW.reservedDate > NOW()
+	THEN SIGNAL SQLSTATE 'HY000'
+		SET MYSQL_ERRNO = 1525,
+        MESSAGE_TEXT = 'reservedDate is in the future';
+	END IF;
+END //
+DELIMITER ; 
 -- -----------------------------------------------------------------------------------------------------
 -- q5: Answer to question 5 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
+INSERT reservedFor VALUES ('Dorit', 'Dorit', '2027-01-13 00:00:00')
--- week13/answers.sql
+++ week13/answers.sql
@@ -1,22 +1,37 @@
 -- In next line, insert your student ID after the colon.
--- Student ID: 
+-- Student ID: s252786
 
 -- Below, you must at most make one SQL query for each question. If you make several, they will be ignored.
-
+USE CompanyDatabase;
 -- -----------------------------------------------------------------------------------------------------
 -- q1: Answer to question 1 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+SELECT ename, dname
+FROM Employee
+WHERE (SELECT country FROM Department WHERE Department.dname = Employee.dname) = 'Denmark' AND
+eid NOT IN (SELECT eid FROM authors);
 -- -----------------------------------------------------------------------------------------------------
 -- q2: Answer to question 2 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+SELECT rid, rname, ryear, COUNT(DISTINCT eid) AS numAuthors
+FROM authors LEFT JOIN Report USING (rid)
+GROUP BY rid, rname, ryear
+HAVING COUNT(DISTINCT eid) >= 2;
 -- -----------------------------------------------------------------------------------------------------
 -- q3: Answer to question 3 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
+SELECT pname, COUNT(DISTINCT rid) AS numReports
+FROM Project LEFT JOIN participates USING (pname) LEFT JOIN authors USING (eid)
+GROUP BY pname;
 -- -----------------------------------------------------------------------------------------------------
 -- q4: Answer to question 4 MUST follow below. (don't edit this line)
 -- -----------------------------------------------------------------------------------------------------
-
-
+DELIMITER //
+CREATE FUNCTION reports (veid INTEGER(3)) RETURNS INTEGER
+BEGIN
+	DECLARE vNumReports INT;
+    SELECT COUNT(*) INTO vNumReports FROM authors
+    WHERE eid = veid;
+	RETURN vNumReports;
+END //
+DELIMITER ;
